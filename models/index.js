a52e7e90a07c7299391e1d0d2aa4a8e29ae28a44
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-24 20:42:35 -0500

    Add relationship between User and Comment

--- models/index.js
+++ models/index.js
@@ -2,12 +2,26 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'gallery_id',
+// One to Many relationship between User and Post
+User.hasMany(Post, {
+  foreignKey: 'user_id',
 });
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
 });
+
+// One to Many relationship between User and Comment
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+
+
+
 
 module.exports = { User, Post, Comment };
