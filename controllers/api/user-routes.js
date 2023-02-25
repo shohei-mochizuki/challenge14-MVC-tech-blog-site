802ebb7a0ef2fcd7c931357bddef8cc08d8bd595
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-25 00:54:08 -0500

    Create user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// CREATE new user
+// Create new user
 router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
@@ -28,33 +28,28 @@ router.post('/login', async (req, res) => {
   try {
     const dbUserData = await User.findOne({
       where: {
-        email: req.body.email,
+        username: req.body.username,
       },
     });
-
     if (!dbUserData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
+        .json({ message: 'Incorrect username. Please try again!' });
       return;
     }
-
     const validPassword = await dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
+        .json({ message: 'Incorrect password. Please try again!' });
       return;
     }
-
+    const userId = dbUserData.get({ plain: true }).user_id;
     req.session.save(() => {
       req.session.loggedIn = true;
-
-      res
-        .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
+      req.session.user_id = userId;
     });
+    res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
