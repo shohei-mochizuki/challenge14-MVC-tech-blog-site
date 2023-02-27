8ccb0176976c2a46027e2513590be2d975a804c5
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 01:26:00 -0500

    Finalize edit/delete post functions

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,60 +1,45 @@
-// const post_id = window.location.toString().split('/')[
-//  window.location.toString().split('/').length - 1];
+const post_id = window.location.toString().split('/')[
+ window.location.toString().split('/').length - 1];
 
-// const updatePostHandler = async (event) => {
-//   event.preventDefault();
-
-//   const title = document.querySelector('#title').value.trim();
-//   const content = document.querySelector('#content').value.trim();
-  
-//   if (title && content) {
-//     const response = await fetch(`/api/post/${post_id}`, {
-//       method: 'PUT',
-//       body: JSON.stringify({ title, content }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert('Failed to update the post.');
-//     }
-//   }
-// };
-
-// const deletePostHandler = async (event) => {
-//   event.preventDefault();
-//   console.log("HERE!");
-  // const response = await fetch(`/api/post/${post_id}`, {
-  //   method: 'DELETE',
-  //   // body: JSON.stringify({ username, email, password }),
-  //   // headers: { 'Content-Type': 'application/json' },
-  //   });
-  //   if (response.ok) {
-  //     document.location.replace('/dashboard');
-  //   } else {
-  //     alert('Failed to delete the post.');
-  //   }
-  // };
-
-// document
-//   .querySelector('#updateBtn')
-//   .addEventListener('submit', updatePostHandler);
-
-function deletePostHandler(event){
+const updatepostFormHandler = async (event) => {
   event.preventDefault();
-  console.log("HERE!");
+
+  const title = document.querySelector('#title-updatepost').value.trim();
+  const content = document.querySelector('#content-updatepost').value.trim();
+
+  if (title && content) {
+    const response = await fetch(`/api/post/${post_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to update a post.');
+    }
+  }
+};
+
+const deletepostFormHandler = async (event) => {
+  event.preventDefault();
+
+  const response = await fetch(`/api/post/${post_id}`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete a post.');
+  }
 };
 
 document
-  .querySelector('#deleteBtn')
-  .addEventListener('submit', deletePostHandler);
+  .querySelector('.updatepost-form')
+  .addEventListener('submit', updatepostFormHandler);
 
-  // Fetchの部分であらかじめpost_idは取得しておき、req.bodyに入れておく
-// const id = window.location.toString().split('/')[
-//  window.location.toString().split('/').length - 1];
-// method: 'POST',
-// body: JSON.stringify({
-//  content,
-//  creation_date,
-//  post_id,
+document
+.querySelector('.deletepost-form')
+.addEventListener('submit', deletepostFormHandler);
