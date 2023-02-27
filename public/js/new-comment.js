5d17c51734fe3915fc99d6ace119bb063f8a52d2
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 01:36:26 -0500

    Update new-comment.js

--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -1,50 +1,26 @@
-const loginFormHandler = async (event) => {
+const post_id = window.location.toString().split('/')[
+  window.location.toString().split('/').length - 1];
+
+const newcommentFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const content = document.querySelector('#content-newcomment').value.trim();
 
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
+  if (content) {
+    const response = await fetch(`/api/comment/${post_id}`, {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ content }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert('Failed to log in.');
-    }
-  }
-};
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to sign up.');
+      alert('Failed to submit a comment.');
     }
   }
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector('.newcomment-form')
+  .addEventListener('submit', newcommentFormHandler);
