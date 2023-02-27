6999ba42cb110060efbfbf51cfde8e19d045b866
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 15:29:07 -0500

    Set the browser reload the page when user submits a new comment

--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -14,7 +14,7 @@ const newcommentFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.reload();
     } else {
       alert('Failed to submit a comment.');
     }
