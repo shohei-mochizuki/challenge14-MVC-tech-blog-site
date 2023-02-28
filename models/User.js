afa78ccde4ece04dce14be97fc040f28b3113015
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 19:46:22 -0500

    Add comments to User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 
 class User extends Model {
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    return bcrypt.compareSync(loginPw, this.password); // Instance method to check password (user input vs database)
   }
 }
 
@@ -25,13 +25,13 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [8], // Minimum length of 8 characters
       },
     },
   },
   {
     hooks: {
-      async beforeCreate(newUserData) {
+      async beforeCreate(newUserData) { // Use bcrypt to encrypt passwords before storing them in the database
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
