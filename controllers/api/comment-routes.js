957fac085c072784d9b41c8df51c0fbc8657a7ad
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 19:55:51 -0500

    Add comments to comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-// CREATE new comment
+
+// Create new comment
 router.post('/', async (req, res) => {
   try {
     const dbCommentData = await Comment.create({
@@ -9,11 +10,11 @@ router.post('/', async (req, res) => {
       post_id: req.body.post_id,
       user_id: req.session.user_id,
     });
-      res.status(200).json(dbCommentData);
+    res.status(200).json(dbCommentData);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
+
 
 module.exports = router;
