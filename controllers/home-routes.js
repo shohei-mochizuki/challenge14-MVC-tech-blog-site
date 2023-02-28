602db04bb7257ee185c0e982be38345f9a5fb57e
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 19:51:35 -0500

    Add comments to home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth'); // Custom middleware for authentification
 
+
 // GET for homepage ('/')
 router.get('/', async (req, res) => {
   try {
@@ -19,6 +20,7 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 
 // GET for posts page ('/post/:id')
 router.get('/post/:id', async (req, res) => {
@@ -40,8 +42,9 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+
 // GET for dashboard page ('/dashboard')
-router.get('/dashboard', withAuth, async (req, res) => { // withAuth
+router.get('/dashboard', withAuth, async (req, res) => { // withAuth: only if user is logged in, the callback function is executed
   try {
     const dbPostData = await Post.findAll({
       include: [{
@@ -53,43 +56,24 @@ router.get('/dashboard', withAuth, async (req, res) => { // withAuth
       },
     });
     const posts = dbPostData.map((post) => post.get({ plain: true }));
-
-    // LATER
     res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
-    // res.status(200).json(posts)
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-// GET for add new post page ('/dashboard/newpost')
-router.get('/dashboard/newpost', withAuth, (req, res) => { // withAuth
 
-  // LATER
+// GET for add new post page ('/dashboard/newpost')
+router.get('/dashboard/newpost', withAuth, (req, res) => { // withAuth: only if user is logged in, the callback function is executed
   res.render('newpost', { loggedIn: req.session.loggedIn });
-  // res.status(200).json('Dashboard new post page!');
 });
 
+
 // GET for edit/delete post page ('/dashboard/post/:id')
-router.get('/dashboard/post/:id', withAuth, async (req, res) => { // withAuth
+router.get('/dashboard/post/:id', withAuth, async (req, res) => {  // withAuth: only if user is logged in, the callback function is executed
   try {
-    const dbPostData = await Post.findByPk(req.params.id, {
-      // include: [
-      //   {
-      //     model: Painting,
-      //     attributes: [
-      //       'id',
-      //       'title',
-      //       'artist',
-      //       'exhibition_date',
-      //       'filename',
-      //       'description',
-      //     ],
-      //   },
-      // ],
-    });
+    const dbPostData = await Post.findByPk(req.params.id, {});
     const post = dbPostData.get({ plain: true });
     res.render('editpost', { post, loggedIn: req.session.loggedIn });
   } catch (err) {
@@ -98,18 +82,17 @@ router.get('/dashboard/post/:id', withAuth, async (req, res) => { // withAuth
   }
 });
 
+
 // GET for signup page ('/signup')
 router.get('/signup', (req, res) => {
-  // LATER
   res.render('signup', { loggedIn: req.session.loggedIn });
-  // res.status(200).json('Signup page!');
 });
+
 
 // GET for login page ('/login')
 router.get('/login', (req, res) => {
-  // LATER
   res.render('login', { loggedIn: req.session.loggedIn });
-  // res.status(200).json('Login page!');
 });
+
 
 module.exports = router;
