762ead036f96b0388f1f7e7771893db78d2a89d8
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-27 19:28:53 -0500

    Add comments to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,5 @@
-const { User } = require('../models');
-
 module.exports = {
-  format_date: (date) => {
+  format_date: (date) => { // Format date information to MM/DD/YYYY
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
   },
 };
